1332f2aae24003e9164b5003c8c187b9ae61cb0a
maomao09198 <rojakorim42@gmail.com>
2025-04-17 16:21:31 +0600

    your message here

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter> {/* 👈 CHANGE THIS */}
-      <Blog/>
+    
     </HashRouter>
   </React.StrictMode>
 )
